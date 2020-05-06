f3b438b18821fec476dd0b67f7b7e93b70f41630
hsp@einfo-tech.com <hsp@einfo-tech.com>
2020-05-06 18:33:35 +0800

    git

--- hsp_no_quick_create/__manifest__.py
+++ hsp_no_quick_create/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2019 EINFO HSP
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Relative field no quick create by HSP",
+    'name': "Relative field forbid quick create by HSP",
 
     'summary': "屏蔽many2one many2many 等直接创建按钮",
    'license': 'LGPL-3',
