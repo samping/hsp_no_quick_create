909950492e6f86dbce3a3b4488896212fff2e0b5
hsp@einfo-tech.com <hsp@einfo-tech.com>
2020-05-06 18:05:44 +0800

    git

--- hsp_no_quick_create/__manifest__.py
+++ hsp_no_quick_create/__manifest__.py
@@ -12,7 +12,7 @@
     'website': "https://www.garage-kit.com",
     'images': ['static/description/logo.png'],
     'category': 'Reporting',
-    'version': '13.0.1.0.0',
+    'version': '12.0.1.0.0',
   
     'depends': [
         'base', 'web',
